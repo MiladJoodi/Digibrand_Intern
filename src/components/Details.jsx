4b41b6fd60b1f98f3bdab4bd38434a6f3c08fbd7
Milad Joodi <winbeta.com@gmail.com>
2024-07-19 15:55:03 +0330

    Update Details.jsx

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -5,11 +5,11 @@ import { useState } from "react";
 import clsx from "clsx";
 
 const data = [
-  { id: 1, color: "red-400" },
-  { id: 2, color: "zinc-200" },
-  { id: 3, color: "indigo-200" },
-  { id: 4, color: "violet-200" },
-  { id: 5, color: "stone-200" },
+  { id: 1, color: "bg-red-400" },
+  { id: 2, color: "bg-zinc-200" },
+  { id: 3, color: "bg-indigo-200" },
+  { id: 4, color: "bg-violet-200" },
+  { id: 5, color: "bg-stone-200" },
 ];
 
 const Details = () => {
@@ -18,12 +18,13 @@ const Details = () => {
   return (
     <div>
       <p>پاکت کرافت 27cm x 36cm بسته 25 عددی</p>
-      <div className="flex gap-5">
+      {/* Select Color */}
+      <div className="flex gap-6">
         {data.map((item) => {
           return (
             <button
               onClick={() => setSelectedColor(item.id)}
-              className={`w-5 h-5 rounded-full bg-${item.color} ${
+              className={`w-5 h-5 rounded-full ${item.color} ${
                 selectedColor == item.id
                   ? `ring-1 ring-opacity-50 ring-offset-2 ring-slate-400`
                   : ""
@@ -32,6 +33,19 @@ const Details = () => {
             ></button>
           );
         })}
+      </div>
+
+      {/* Price */}
+      <div className="flex gap-4">
+        <p>قیمت هر کیلو:</p>
+        <p>94,300 تومان</p>
+      </div>
+
+      {/* Level */}
+      <div className="flex text-sm">
+        <p>سطح رضایت خریداران:</p>
+        <p>(98%) عالی</p>
+        <p>21 دیدگاه ثبت شده</p>
       </div>
     </div>
   );
